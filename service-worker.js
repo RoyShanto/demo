c1037961f88102f9a6c84f46a405a72fc9bc1341
Shanto Roy <roycse17@gmail.com>
2026-03-13 19:17:37 +0600

    test

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "demo-cache-";
-const VERSION = "v12";
+const VERSION = "v13";
 
 const CACHE_NAME = CACHE_PREFIX + VERSION;
 
@@ -11,40 +11,51 @@ const urlsToCache = [
   "/demo/manifest.json"
 ];
 
+
 // INSTALL
 self.addEventListener("install", (event) => {
+
   self.skipWaiting();
 
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
   );
+
 });
 
 
 // ACTIVATE
 self.addEventListener("activate", (event) => {
+
   event.waitUntil(
     caches.keys().then((keys) => {
+
       return Promise.all(
         keys.map((key) => {
+
           // delete only this project's old caches
           if (key.startsWith(CACHE_PREFIX) && key !== CACHE_NAME) {
             return caches.delete(key);
           }
+
         })
       );
+
     })
   );
 
   self.clients.claim();
+
 });
 
 
 // FETCH
 self.addEventListener("fetch", (event) => {
 
-  // HTML pages → NETWORK FIRST
+  // NETWORK FIRST for HTML
   if (event.request.mode === "navigate") {
 
     event.respondWith(
@@ -69,7 +80,7 @@ self.addEventListener("fetch", (event) => {
   }
 
 
-  // STATIC FILES → CACHE FIRST
+  // CACHE FIRST for assets
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
